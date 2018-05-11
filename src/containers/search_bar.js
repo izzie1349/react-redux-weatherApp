60bfd08a38aa5768a64c94000aeb2bfa620592ff
Israel Alvarez <ialvarez@nyc01alee.local>
2018-05-11 14:39:13 -0400

    bind callback

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,13 +5,25 @@ export default class SearchBar extends Component {
         super(props);
 
         this.state = { term: '' };
+
+        // this is an instance of seachbar
+        // SearchBar has a function onInputChange
+        // bind that function to this which is searhcbar
+        // and replace searchbar with new bound instance of searchbar
+        this.onInputChange = this.onInputChange.bind(this);
     }
 
 
     onInputChange(event) {
         console.log(event.target.value);
+        this.setState({ term: event.target.value });
     }
 
+
+
+    // * rule of thumb:
+    // when you have a callback that makes a reference to this,
+    // you need to bind it
     render() {
         return (
             <form className="input-group">
