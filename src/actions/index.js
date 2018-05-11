d2a35d9a091b07958aa012d132c7d1d0b6f35d66
Israel Alvarez <ialvarez@nyc01alee.local>
2018-05-11 16:14:50 -0400

    ajax request using axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = 'c36e95caa11f415c8bc4d315613da8f6';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `{ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+
+    return {
+        type: FETCH_WEATHER,
+        // additional data
+        payload: request
+    };
+}
